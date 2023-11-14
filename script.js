60a91a44ea75488d48855bc1768f90c0495165b6
fahad471 <72732418+fahad471@users.noreply.github.com>
2023-11-14 14:17:21 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,65 @@
-document.getElementById("openLinksButton").addEventListener("click", function() {
+//document.getElementById("openLinksButton").addEventListener("click", function() {
     // Define the URLs you want to open
     
-    var link1 = "./Vielen_dank.html";
+ //   var link1 = "./Vielen_dank.html";
 
     // Open the first link in a new tab
-    window.open(link1, "_blank");
+  //  window.open(link1, "_blank");
 
     // Open the second link in a new tab
     
-});
+//});
 
+// var forms = document.querySelectorAll(".needs-validation");
+// Array.prototype.slice.call( forms ).forEach( function( form )
+// {
+//     form.addEventListener( "submit" , function( event )
+//     {
+//         if ( !form.checkValidity( ))
+//         {
+//         event.preventDefault( );
+//         event.stopPropagation( );
+        
+//         }
+//         form.classList.add( "was-validated" );
+        
+//     }, false);
+// });
 var forms = document.querySelectorAll(".needs-validation");
-Array.prototype.slice.call( forms ).forEach( function( form )
-{
-    form.addEventListener( "submit" , function( event )
-    {
-        if ( !form.checkValidity( ))
-        {
-        event.preventDefault( );
-        event.stopPropagation( );
+
+Array.prototype.slice.call(forms).forEach(function(form) {
+    form.addEventListener("submit", function(event) {
+        if (!form.checkValidity()) {
+            // Form is not valid, prevent default form submission
+            event.preventDefault();
+            event.stopPropagation();
+        } else {
+            // Form is valid, allow default form submission
+            // This will submit the form to the specified action
+            
+            // Your logic to perform any additional actions after submission
+            // (if needed before redirection)
+
+            // Redirect to the thank you page
+            var link1 = "./Vielen_dank.html";
+            
+
+
+    // Open the first link in a new tab
+    window.open(link1, "_blank");
+    
+        window.close();
+    
         }
-        form.classList.add( "was-validated" );
+
+        form.classList.add("was-validated");
     }, false);
 });
+
+
+
+
+
+
 
 
